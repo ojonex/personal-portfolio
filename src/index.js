a10908f542a750322c61f0dd693d98ea899852c0
OdinDev <odinDevs@proton.me>
2024-09-13 20:59:22 +0100

    install prettier and format code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-const Pet = () => {
-    return React.createElement(
-        "div", {}, [
-            React.createElement("h1", {}, "Harley"),
-            React.createElement("h1", {}, "Dog"),
-            React.createElement("h1", {}, "Husky x Collie"),
-
-        ]
-    )
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed),
+  ]);
+};
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Whats going on gamers"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
-        ]
-
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Whats going on gamers"),
+    React.createElement(Pet, {
+      name: "Harry",
+      animal: "dinosaur",
+      breed: "t-rex",
+    }),
+    React.createElement(Pet, {
+      name: "Harry",
+      animal: "dinosaur",
+      breed: "t-rex",
+    }),
+    React.createElement(Pet, {
+      name: "Harry",
+      animal: "dinosaur",
+      breed: "t-rex",
+    }),
+  ]);
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
