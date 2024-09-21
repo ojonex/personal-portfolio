e37998a19b29ada1401e1ebbe4e615e0890ee2c8
OdinDev <odinDevs@proton.me>
2024-09-21 19:16:20 +0100

    set-up tailwind for production, include favicon to fix 404

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/{html,js,css}"],
+  content: [
+    "./src/**/*.jsx", // Include all .jsx files in src directory
+    "./index.html", // Include index.html in the root
+  ],
   theme: {
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
